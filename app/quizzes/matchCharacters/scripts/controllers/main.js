04bb337cdfee27ea2e7891fd02914d5c1de3111e
redaemn <gabriele.rabbiosi@gmail.com>
2014-03-12 00:15:43 +0100

    feat(quiz-match-characters): implement a stub that just shows images and names

--- app/quizzes/matchCharacters/scripts/controllers/main.js
+++ app/quizzes/matchCharacters/scripts/controllers/main.js
@@ -2,10 +2,30 @@
 
 angular.module('marvelQuizApp.quizzes')
   .controller('mq.matchCharacters.MainCtrl', function ($scope, MarvelData) {
-    $scope.name = 'world';
 
-    MarvelData.getRandomCharacter()
-      .then(function(totalNum) {
-        $scope.name = totalNum;
+    // maps index of images inside array with index of names
+    // var imagesNamesMap = [];
+
+    $scope.characters = [];
+
+    MarvelData.getRandomCharacter({
+      count: 3,
+      withImageAvailable: true
+    }).then(function(characters) {
+      angular.forEach(characters, function (character) {
+        $scope.characters.push({
+          image: buildImageUrl(character.thumbnail),
+          name: character.name
+        });
       });
+    });
+
+    // from a thumbnail object returned by the service create a url
+    // to be used inside <img> src attribute (put this inside a utility service)
+    function buildImageUrl(thumbnailObj) {
+      var url = thumbnailObj.path;
+      url += '/portrait_fantastic.';
+      url += thumbnailObj.extension;
+      return url;
+    }
   });
