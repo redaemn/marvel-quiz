d1d7f818256ed1d33f871e2ebf6249ea44380ea0
redaemn <gabriele.rabbiosi@gmail.com>
2014-03-15 20:43:39 +0100

    feat(cache): implement the possibility to define a specific expiration time for a cache item

--- app/scripts/services/cache.js
+++ app/scripts/services/cache.js
@@ -5,10 +5,7 @@
  */
 
 angular.module('marvelQuizApp.common')
-  .service('Cache', function Cache($window) {
-
-    // IMPROVEMENT: I could use local storage to improve the cache duration time
-    // but then I should think about a strategy to garbage-collect old items
+  .service('Cache', function Cache($window, $timeout) {
 
     // TODO: make this service a provider and make it possible to configure
     // cache duration time
@@ -21,22 +18,35 @@ angular.module('marvelQuizApp.common')
      * Create a cache item
      *
      * @param {Object} data
-     * @param {Date} [created]
+     * @param {Date} [created] - the date when this item was created
+     * @param {Number} [expiration] - number of milliseconds after which the
+     *   data is considered expired; default is 24 hours; -1 means thet it
+     *   never expires
      */
-    function CacheItem(data, created) {
+    function CacheItem(data, created, expiration) {
       this.data = data;
+
       if (created) {
         this.created = new Date(created);
       }
       else {
         this.created = new Date();
       }
+
+      if (expiration) {
+        this.expiration = expiration;
+      }
     }
 
     CacheItem.prototype.isExpired = function () {
       var now = new Date();
 
-      return now - this.created > 1000 * 60 * 60 * 24; // 24 hours
+      if (this.expiration === -1) {
+        return false;
+      }
+      else {
+        return now - this.created > ( this.expiration || 1000 * 60 * 60 * 24); // 24 hours
+      }
     };
 
     /*
@@ -51,7 +61,7 @@ angular.module('marvelQuizApp.common')
       else {
         cache = {};
         angular.forEach(angular.fromJson(jsonCache), function(val, key) {
-          var cacheItem = new CacheItem(val.data, val.created);
+          var cacheItem = new CacheItem(val.data, val.created, val.expiration);
 
           if (!cacheItem.isExpired()) {
             cache[key] = cacheItem;
@@ -71,11 +81,13 @@ angular.module('marvelQuizApp.common')
     }
 
     /*
-     * Persist cache to browser storage
+     * Asynchronously persist cache to browser storage
      */
     function _persistMarvelCache(cache) {
       if (storage) {
-        storage.setItem('MarvelQuizApp', angular.toJson(cache));
+        $timeout(function() {
+          storage.setItem('MarvelQuizApp', angular.toJson(cache));
+        });
       }
     }
 
@@ -84,9 +96,13 @@ angular.module('marvelQuizApp.common')
      *
      * @param {String} key
      * @param {Object} data
+     * @param {Number} [expiration] - number of milliseconds after which the
+     *   data is considered expired; default is 24 hours
      */
-    function put(key, data) {
-      cache[key] = new CacheItem(data);
+    function put(key, data, expiration) {
+      cache[key] = angular.isDefined(expiration) ?
+        new CacheItem(data, null, expiration) :
+        new CacheItem(data);
       _persistMarvelCache(cache);
     }
 
