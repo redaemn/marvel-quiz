16b257d9befd413a52755b22736cd565b7e69016
redaemn <gabriele.rabbiosi@gmail.com>
2014-03-15 20:44:45 +0100

    feat(score): save score to browser storage

--- app/scripts/services/score.js
+++ app/scripts/services/score.js
@@ -5,34 +5,63 @@
  */
 
 angular.module('marvelQuizApp')
-  .service('Score', function Score() {
+  .service('Score', function Score(Cache) {
 
-    // TODO: get this info from local storage
-    var attemptedQuizzes = 0,
-      correctAnswers = 0,
-      wrongAnswers = 0;
+    var scoreValues = Cache.get('scoreValues') || {
+      attemptedQuizzes: 0,
+      correctAnswers: 0,
+      wrongAnswers: 0
+    };
+
+    function _saveScoreValues() {
+      Cache.put('scoreValues', scoreValues, -1);
+    }
 
     // TODO: for every function, handle the possibility to register data grouped by quiz
+
+    /*
+     * Return the current value of attempted quizzes
+     */
     function getAttemptedQuizzes() {
-      return attemptedQuizzes;
+      return scoreValues.attemptedQuizzes;
     }
 
+    /*
+     * Increment the number of attempted quizzes and return the new value
+     */
     function registerAttemptedQuiz() {
-      return ++attemptedQuizzes;
+      var newValue = ++scoreValues.attemptedQuizzes;
+      _saveScoreValues();
+      return newValue;
     }
 
+    /*
+     * Increment the number of correct answers and return the new value
+     */
     function registerCorrectAnswer() {
-      return ++correctAnswers;
+      var newValue = ++scoreValues.correctAnswers;
+      _saveScoreValues();
+      return newValue;
     }
 
+    /*
+     * Increment the number of wrong answers and return the new value
+     */
     function registerWrongAnswer() {
-      return ++wrongAnswers;
+      var newValue = ++scoreValues.wrongAnswers;
+      _saveScoreValues();
+      return newValue;
     }
 
+    /*
+     * Calculate and return the current success rate
+     */
     function successRate() {
       var rate;
 
-      rate = attemptedQuizzes !== 0 ? correctAnswers / attemptedQuizzes : 0;
+      rate = scoreValues.attemptedQuizzes !== 0 ?
+        scoreValues.correctAnswers / scoreValues.attemptedQuizzes :
+        0;
 
       return (rate * 100).toFixed(2);
     }
