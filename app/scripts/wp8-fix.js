c45835e1a14b72796fd96a35b820677d0fa994ba
redaemn <gabriele.rabbiosi@gmail.com>
2014-03-26 20:53:05 +0100

    layout: fix to make responsive layout work on Windows Phone

---
+++ app/scripts/wp8-fix.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function() {
+
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style');
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    );
+    document.querySelector('head').appendChild(msViewportStyle);
+  }
+
+} () );
