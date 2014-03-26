c2280556371dae680d4b379103e6c7705300628f
redaemn <gabriele.rabbiosi@gmail.com>
2014-03-26 21:45:14 +0100

    feat(HomeCtrl): remove unused code

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,10 +3,4 @@
 angular.module('marvelQuizApp')
   .controller('HomeCtrl', function ($scope, GoogleAnalytics) {
     GoogleAnalytics.pageView();
-
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
